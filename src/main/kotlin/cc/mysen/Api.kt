eab4e78518b12c45a5373b4c026934d0d35cbac5
Kristoffer Høegh Mysen <kristoffer.mysen@gmail.com>
2019-10-22 21:40:30 +0200

    Got POST file enpoint to work

--- src/main/kotlin/cc/mysen/Api.kt
+++ src/main/kotlin/cc/mysen/Api.kt
@@ -1,6 +1,11 @@
 package cc.mysen
 
 import spark.kotlin.post
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+import javax.servlet.MultipartConfigElement
+
 
 fun main() {
 
@@ -8,6 +13,17 @@ fun main() {
         println("Got a http request!")
         val message = request.body()
         println(message)
+    }
+
+    post("/api/v1/transactions/file", accepts = "multipart/form-data") {
+        request.attribute("org.eclipse.jetty.multipartConfig", MultipartConfigElement(System.getProperty("java.io.tmpdir")));
+
+        val uploadDir = File("upload")
+        uploadDir.mkdir()
+        val tempFile = Files.createTempFile(uploadDir.toPath(), "", "")
+
+        request.raw().getPart("uploaded_file").inputStream.use { input ->
+            Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING) }
     }
 
 }
