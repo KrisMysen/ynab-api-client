542f890eb6db4905a54c25033ff5c28ff69a4804
Kristoffer Høegh Mysen <kristoffer.mysen@gmail.com>
2020-01-01 18:34:17 +0100

    Created regex matching and destructuring for gjensidige transactions

--- src/main/kotlin/cc/mysen/transactions/GjensidigeTransactionReader.kt
+++ src/main/kotlin/cc/mysen/transactions/GjensidigeTransactionReader.kt
@@ -6,10 +6,22 @@ import java.util.stream.Collectors
 
 class GjensidigeTransactionReader {
 
+    companion object {
+        val regex = "([0-9]{2}\\.[0-9]{2}\\.[0-9]{4})\\s(.+)=\"(.+)\"\\s(-?\\d+\\.?\\d+)".toRegex()
+    }
+
+
     fun read(input: InputStream): List<Transaction> {
         val reader = BufferedReader(input.reader())
-        return reader.lines().map { Transaction("", "", 0F) }
+        return reader.lines().map { lineToTransaction(it) }
             .collect(Collectors.toList())
     }
+
+    private fun lineToTransaction(line: String): Transaction {
+        return regex.matchEntire(line)?.destructured?.
+            let { (date, type, description, amount) -> Transaction(date, description, 0F) }
+            ?: Transaction.invalid()
+    }
+
 
 }
--- src/main/kotlin/cc/mysen/transactions/Transaction.kt
+++ src/main/kotlin/cc/mysen/transactions/Transaction.kt
@@ -4,4 +4,13 @@ data class Transaction(
     val date: String,
     val description: String,
     val amount: Float
-)
+) {
+
+    companion object Invalid {
+        fun invalid(): Transaction {
+            return Transaction("", "", 0F)
+        }
+    }
+
+
+}
